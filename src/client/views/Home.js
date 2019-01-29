e04375da5515766d7bdcba6c3d3286907d4fcc1c
Varun <varun@Varuns-iMac.local>
2019-01-29 15:20:03 +0530

    Wireframe

--- src/client/views/Home.js
+++ src/client/views/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Row, Col } from 'react-simple-flex-grid';
+import "react-simple-flex-grid/lib/main.css";
+
+
 
 function Home(/* props */) {
-  return <div>Lets kick start the rockets.
+  return <div>Lets kick start the rockets. yo
 
 <Link to={`/about`}>About</Link>
+<Row gutter={40}>
+   <Col span={4}>clear
+   </Col>
+   <Col span={4}>col-4</Col>
+   <Col span={4}>col-4</Col>
+</Row>
 
   </div>;
 }
